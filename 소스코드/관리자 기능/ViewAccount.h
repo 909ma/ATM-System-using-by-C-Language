e5efde131bfe0e6f54122cdc09af3a87fb71ce1b
Kim Sinhyuk <sin00021@naver.com>
2023-03-05 04:05:10 +0900

    add

---
+++ 소스코드/관리자 기능/ViewAccount.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+ViewAccount(int a)함수는 people.csv 파일에서 a만큼의 인원수를 읽어옵니다.
+최대 100명까지 관리할 수 있습니다. 
+*/ 
+#include"PersonStruct.h"
+void read_csv_file(char* filename, Person* people, int max_num_people) {
+    FILE* fp = fopen(filename, "r");    // 파일을 읽기 모드로 열기
+    if (fp == NULL) {
+        printf("File open error!\n");
+        return;
+    }
+
+    char buffer[1024];    // 한 줄씩 읽어올 버퍼
+    int count = 0;        // 읽어온 사람 수
+
+    while (fgets(buffer, 1024, fp)) {    // 한 줄씩 읽어오기
+        if (count >= max_num_people) {   // 최대 읽어올 수 있는 사람 수를 초과하면 종료
+            break;
+        }
+        char* BankName = strtok(buffer, ",");                    // BankName 읽어오기
+        int BankCode = atoi(strtok(NULL, ","));                 // BankCode 읽어오기
+        int ClientCode = atoi(strtok(NULL, ","));              // ClientCode 읽어오기
+        char* ClientName = strtok(NULL, ",");                 // ClientName 읽어오기
+        int CardNumber = atoi(strtok(NULL, ","));            // CardNumber 읽어오기
+        int AccountNumber = atoi(strtok(NULL, ","));        // AccountNumber 읽어오기
+        int NowMoney = atoi(strtok(NULL, ","));            // NowMoney 읽어오기
+        int AccountPW = atoi(strtok(NULL, ","));          // AccountPW 읽어오기
+        int AccountDate = atoi(strtok(NULL, ","));       // AccountDate 읽어오기
+
+        strcpy(people[count].BankName, BankName);
+        people[count].BankCode = BankCode;
+        people[count].ClientCode = ClientCode;
+        strcpy(people[count].ClientName, ClientName);
+        people[count].CardNumber = CardNumber;
+        people[count].AccountNumber = AccountNumber;
+        people[count].NowMoney = NowMoney;
+        people[count].AccountPW = AccountPW;
+        people[count].AccountDate = AccountDate;
+
+        count++;
+    }
+
+    fclose(fp);    // 파일 닫기
+}
+int ViewAccount(int a) {
+    Person* people = malloc(sizeof(Person) * 100);// 최대 100명의 사람 정보를 저장할 수 있는 배열 동적 할당
+
+    read_csv_file("people.csv", people, a+1);//a의 인원수를 조회 
+
+    for (int i = 0; i < a+1; i++) {
+        printf("Bank Name: %s, Bank Code: %d, Clinet Code: %d, ClientName: %s, CardNumber: %d, AccountNumber: %d, NowMoney: %d, AccountPW: %d, AccountDate: %d\n", people[i].BankName, people[i].BankCode, people[i].ClientCode,people[i].ClientName,people[i].CardNumber,people[i].AccountNumber,people[i].NowMoney,people[i].AccountPW,people[i].AccountDate);
+    }
+    free(people);// 동적 할당된 메모리 해제
+
+    return 0;
+}
+
---
+++ 소스코드/관리자 기능/test 코드.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#include"ViewAccount.h"
+#include"CreatAccount.h"
+#include"MergeAccount.h"
+#include"NameChange.h"
+//PersonStruct.h 파일은 필요하니 신경써주세요 
+
+int main(){
+	ViewAccount(15);//데이터 peple.csv 파일에 있는 명단 중 상위 15명의 정보를 조회합니다. 
+	
+	CreatAccount();//계좌개설의 기능을 합니다. 여기에서 입력을 하면 new_people.csv 파일로 만들어집니다. 
+	MergeAccount();//new_people.csv의 데이터를 people.csv파일와 병합하고 merged.csv파일을 만듭니다. 
+	NameChange();//병합하고 난 뒤 오래된 정보를 가지고 있는 people.csv파일을 지우고 merged.csv파일을 people.csv파일로 바꿉니다. 
+
+	return 0;
+}
