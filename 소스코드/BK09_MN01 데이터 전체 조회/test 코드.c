08eb08e4802a397a70b87410c83b3c773cb25af9
Kim Sinhyuk <sin00021@naver.com>
2023-03-06 16:31:11 +0900

    BK09_MN01

---
+++ 소스코드/BK09_MN01 데이터 전체 조회/test 코드.c
@@ -0,0 +1,9 @@
+#pragma warning(disable: 4996)
+#include<stdio.h>
+
+#include"ViewAccount.h"
+
+int main(){
+	ViewAccount(99);//데이터 people.csv 파일에 있는 명단 중 상위 10명의 정보를 조회합니다. 최대 100?명까지 받을 수 있습니다.
+	return 0;
+}
