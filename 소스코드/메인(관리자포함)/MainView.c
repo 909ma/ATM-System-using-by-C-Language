ee514675709b91c435dc52734627a4e7ad3c7b21
Jangdaegun <jdg8988@naver.com>
2023-03-07 10:46:08 +0900

    메인뷰 및 관리자 뷰(기능연동중)

---
+++ 소스코드/메인(관리자포함)/MainView.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<conio.h>
+#include"ViewAccount.h"
+#include"CreatAccount.h"
+#include"withdraw.h"
+#include"Menu.h"
+
+void admin()
+{
+	int MenuNum;
+	int menuSelect;
+	Person* people = malloc(sizeof(Person) * 100);
+RESHOW:
+	showAdminMenu();
+RE:
+	printf("원하는 기능 선택 : ");
+	scanf("%d", &MenuNum);
+	switch (MenuNum)
+	{
+	case 1:
+		ViewAccount(90);		
+		printf("1. 이전메뉴 돌아가기\t 2. 사용 종료\n\n");
+		printf("선택 : ");
+		scanf("%d", &menuSelect);
+		if (menuSelect == 1)
+		{
+			system("cls");
+			goto RESHOW;
+		}
+		else
+			printf("사용종료\n");
+		break;
+	case 2:
+		CreatAccount();
+		printf("1. 이전메뉴 돌아가기\t 2. 사용 종료\n\n");
+		printf("선택 : ");
+		scanf("%d", &menuSelect);
+		if (menuSelect == 1)
+		{
+			system("cls");
+			goto RESHOW;
+		}
+		else
+			printf("사용종료\n");
+		break;
+	case 3:
+		break;
+	case 4:
+		printf("사용을 종료합니다\n");
+		break;
+	default:
+		printf("다시 입력해주세요\n");
+		goto RE;
+	}
+}
+int main()
+{
+	int MenuNum;
+	Person* people = malloc(sizeof(Person) * 100);
+RESHOW:
+	showMenu();
+RE:
+	printf("원하는 기능 선택 : ");
+	scanf("%d", &MenuNum);
+	switch (MenuNum)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		printf("\n");
+		withdraw("people.csv", people);
+		printf("1. 이전메뉴 돌아가기\t 2. 사용 종료\n\n");
+		int menuSelect;
+		printf("선택 : ");
+		scanf("%d", &menuSelect);
+		if (menuSelect == 1)
+		{
+			system("cls");
+			goto RESHOW;
+		}
+		else
+			printf("사용종료\n");
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		printf("사용을 종료합니다\n");
+		break;
+	case 1111:
+		printf("관리자 모드 접속 완료\n");
+		system("cls");
+		admin();
+		break;
+	default:
+		printf("다시 입력해주세요\n");
+		goto RE;
+	}
+
+	return 0;
+}
