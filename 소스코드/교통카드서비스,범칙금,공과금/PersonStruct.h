57f8dffef8f6cbf4b4175a7e071f7ff757201acd
gsa5000 <angyoseok50@gmail.com>
2023-03-07 16:30:21 +0900

    교통카드 서비스 업데이트

---
+++ 소스코드/교통카드서비스,범칙금,공과금/PersonStruct.h
@@ -0,0 +1,15 @@
+#pragma once
+ 
+#include<stdio.h>
+
+typedef struct {
+    char BankName[20];
+    int BankCode;
+    int ClientCode;
+    char ClientName[20];
+    int CardNumber;
+    int AccountNumber;
+    int NowMoney;
+    int AccountPW;
+    int AccountDate;
+} Person;
--- 소스코드/교통카드서비스,범칙금,공과금/TrafficCard.c
+++ 소스코드/교통카드서비스,범칙금,공과금/TrafficCard.c
@@ -1,53 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "PersonStruct.h"
+#include "search.h"
+#include "change.h"
 
-int TrafficCard(int *cash)
+
+int TrafficCard(int *cash,int code)
 {
 	int option = 0;
-	int dp, wd,code;
+	int dp, wd, select1=0;
+	if (code!=30)
+	{
+		printf("교통카드가 아닙니다\n");
+		return 0;
+	}
+	else
+	{
+		printf("현재잔액은 %d원입니다.\n", *cash);
+		printf("원하시는 메뉴를 선택해주십시오\n");
+		printf(" 1. 충전\n 2. 환불 \n 3. 취소\n");
+		scanf("%d", &option);
+		switch (option)
+		{
+		case 1:
+		{
+			printf("현금을 넣어주세요\n");
+			scanf("%d", &dp);
+			printf("입금중입니다.\n");
+			*cash += dp;
+			break;
+		}
+		case 2:
+		{
+			while (1)
+			{			
+				printf("환불할 금액을 정하세요\n");
+				scanf("%d", &wd);
+				if (wd > *cash)
+				{
+					printf("현금을 출력할수 없습니다.\n다시 입력하시겠습니까?[네:1/아니오:2]\n");
+					scanf("%d", &select1);
+					if (select1 == 1);
+					else if (select1 == 2)
+						break;
+				}
+				else
+				{
+					printf("현금을 출력중입니다.\n");
+					*cash -= wd;
+					break;
+				}
+			}
+			break;
+		}
+		case 3:
+		{
+			printf("거래가 취소되었습니다.\n");
+			break;
+		}
+		}
+		if (option != 3)printf("총 잔액은 %d원입니다.\n", *cash);
+		printf("잠시 후 메인화면으로 돌아갑니다.\n");
+		return 0;
+	}
 	
-	printf("현재잔액은 %d원입니다.\n", *cash);
-	printf("원하시는 메뉴를 선택해주십시오\n");
-	printf(" 1. 충전\n 2. 환불 \n 3. 취소\n");
-	scanf("%d", &option);
-	switch (option)
-	{
-	case 1:
-	{
-		printf("현금을 넣어주세요\n");
-		scanf("%d", &dp);
-		printf("입금중입니다.\n");
-		*cash += dp;
-		break;
-	}
-	case 2:
-	{
-		printf("환불할 금액을 정하세요\n");
-		scanf("%d", &wd);
-		if (wd > *cash)
-		{
-			printf("현금을 출력할수 없습니다.\n다시 입력하시겠습니까?[네/아니오]\n");
-		}
-		else
-		{
-			printf("현금을 출력중입니다.\n");
-			*cash -= wd;
-		}
-		break;
-	}
-	case 3:
-	{
-		printf("거래가 취소되었습니다.\n");
-		break;
-	}
-	}
-	if (option != 3)printf("총 잔액은 %d원입니다.\n", *cash);
-	printf("잠시 후 메인화면으로 돌아갑니다.\n");
-	return 0;
 }
 
 int main()
 {
-	int cash=1000;
-	TrafficCard(&cash);
+	Person person;
+	Person* people = malloc(sizeof(Person) * 100);
+	int a;
+	a=search("people.csv",&person,100);
+	if (a == 0)
+	{
+		TrafficCard(&person.NowMoney, person.BankCode);
+		change("people.csv", people, &person);
+	}
+	else
+		printf("결제가 취소되었습니다.\n");
+
+	free(people);
+
 	return 0;
 }
