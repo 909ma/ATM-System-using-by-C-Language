8a9101b0635afe7f30aa6823a80b18a450eb2f1a
gsa5000 <angyoseok50@gmail.com>
2023-03-10 09:27:39 +0900

    코드 수정

--- 소스코드/교통카드서비스,범칙금,공과금/Fine.c
+++ 소스코드/교통카드서비스,범칙금,공과금/Fine.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "search.h"
+#include "PersonStruct.h"
+#include "change.h"
 
-int Fine(int* cash)
+int Utilities(Person* person)
 {
-	int list;
-	char name[30] = "";
 	int fee;
-	printf("고객 명의\n");
-	scanf("%s", name);
-	printf("%s 고객 리스트\n", name);
-	scanf("%d", &list);
-	printf("%d %s 고객님의 범칙금 납부금액을 입력하세요\n", list, name);
-	scanf("%d", &fee);
-	if (*cash < fee)
-		printf("납부에 실패하였습니다.\n");
+	if (person->BankCode != 41)
+	{
+		printf("범칙금 납부를 할 수 없는 계정입니다.\n");
+		return -1;
+	}
 	else
 	{
-		*cash -= fee;
-		printf("납부가 완료되었습니다.\n");
-		printf("고객 코드: %d\n", list);
-		printf("고객명: %s\n", name);
-		printf("납부액: %d\\\n", fee);
-		printf("잔액: %d\\\n", *cash);
+		printf("%s 고객님의 범칙금 납부금액을 입력하세요\n", person->ClientName);
+		scanf("%d", &fee);
+		if (person->NowMoney < fee)
+			printf("납부에 실패하였습니다.\n");
+		else
+		{
+			person->NowMoney -= fee;
+			printf("납부가 완료되었습니다.\n");
+			printf("고객명: %s\n", person->ClientName);
+			printf("납부액: %d\\\n", fee);
+			printf("잔액: %d\\\n", person->NowMoney);
+		}
+		printf("\n잠시 후 메인 화면으로 돌아갑니다.\n");
+		return 0;
 	}
-	printf("\n잠시 후 메인 화면으로 돌아갑니다.\n");
+}
+
+int main()
+{
+	Person person;
+	Person* people = malloc(sizeof(Person) * 100);
+	int a;
+	a = search("people.csv", &person, 100);
+	if (a == 0)
+	{
+		Utilities(&person);
+		change("people.csv", people, &person);
+	}
+	else
+		printf("결제가 취소되었습니다.\n");
+
+	free(people);
+
 	return 0;
 }
