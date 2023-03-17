b488c83296da9c99ea1747a8e3a16410f5814370
Jangdaegun <jdg8988@naver.com>
2023-03-17 15:16:55 +0900

    extrafunc 수정

--- 소스코드/메인(관리자포함)/ATM_SYSTEM/ExtraFunc.h
+++ 소스코드/메인(관리자포함)/ATM_SYSTEM/ExtraFunc.h
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "GiftStruct.h"
-#include "VoiceCheck.h"
-
 int countFileLines(char* file_name) {
 	// 파일에서 전체 라인 수를 세는 함수
 	FILE* fp;  // 파일 포인터
@@ -75,27 +73,50 @@ int getTotalLine(char* filename) { // csv파일의 총 라인 수를 받는 코드
 	fclose(fp);
 	return(line);
 }
+int crime()
+{
+	int crime;
+ReCrime:
+	printf("검찰, 경찰 금융감독원이나 모르는 사람이 전화로 이체를 요청하였습니까?\n");
+	printf("또는, 대출받기 위해서는 먼저 수수료, 기존 대출금 상환 등이 필요하다며 이체를 요청하였습니까?\n");
+	printf("1. 예 (거래중단)\n");
+	printf("2. 아니오 (계속거래)\n");
+	scanf_s("%d", &crime);
+	if (crime == 1)
+		return 1;
+	else if (crime == 2)
+	{
+		printf("거래가 취소 되었습니다.\n처음부터 다시 시작하여 주십시오.\n");
+		printf("놓고 가시는 물건이 없는지 확인하여 주십시오.\n");
+		printf("이용해주셔서 감사합니다.\n");
+		printf("확인");
+		return 2;
+	}
+	else
+	{
+		printf("다시 입력하세요\n");
+		goto ReCrime;
+	}
+}
 int voicefishing()
 {
 	int answer;
 ReFishing:
-	PlaySound(TEXT(".\\BankVoice\\phishingCheck.wav"), NULL, SND_FILENAME | SND_ASYNC);
-	printf("\n\n\n\n※※※※※※보이스피싱 피해예방 안내※※※※※※\n\n");
-	printf("검찰, 경찰, 금융감독원이나 모르는 사람이 전화로\n");
-	printf("출금 및 이체를 요청하셨습니까?\n\n\n");
-	printf("1. 예\t\t 2. 아니오\n");
-	printf("\n선택 : ");
+	printf("검찰, 경찰 금융감독원이나 모르는 사람이 전화로 이체를 요청하였습니까?\n");
+	printf("또는, 대출받기 위해서는 먼저 수수료, 기존 대출금 상환 등이 필요하다며 이체를 요청하였습니까?\n");
+	printf("1. 예 (거래중단)\n");
+	printf("2. 아니오 (계속거래)\n");
 	scanf_s("%d", &answer);
-	checkKey(answer);
-
 	if (answer == 1)
-	{
-		PlaySound(TEXT(".\\BankVoice\\WorkCancel.wav"), NULL, SND_FILENAME | SND_ASYNC);	
-		
 		return 1;
-	}
 	else if (answer == 2)
+	{
+		printf("거래가 취소 되었습니다.\n처음부터 다시 시작하여 주십시오.\n");
+		printf("놓고 가시는 물건이 없는지 확인하여 주십시오.\n");
+		printf("이용해주셔서 감사합니다.\n");
+		printf("확인");
 		return 2;
+	}
 	else
 	{
 		printf("다시 입력하세요\n");
@@ -105,22 +126,42 @@ ReFishing:
 int BackToMenu()
 {
 	int menuSelect;
-Back:	
+	Back:
 	printf("\n1. 이전메뉴 돌아가기\t 2. 사용 종료\n\n");
 	printf("선택 : ");
 	scanf("%d", &menuSelect);
 	if (menuSelect == 1)
 		return menuSelect;
 	else if (menuSelect == 2)
-	{
-		PlaySound(TEXT(".\\BankVoice\\CheckAndBye.wav"), NULL, SND_FILENAME | SND_ASYNC);
-		Sleep(3000);
 		printf(" \n\n놓고 가시는 물건은 없는지 확인하여 주십시오.\n\t 이용해주셔서 감사합니다. \n\t\tTHANK YOU\n");
-	}
 	else
 	{
 		printf("다시 입력하세요\n");
 		goto Back;
 	}
 		
+}
+int PrintBankCode() {
+	printf("┏━━━━━━━━━━━━━━┳━━━━━━━━━━━┓\n");
+	printf("┃    BankCode  ┃    Name   ┃\n");
+	printf("┣━━━━━━━━━━━━━━╋━━━━━━━━━━━┫\n");
+	printf("┃       10     ┃   K B     ┃\n");
+	printf("┃       11     ┃   iBK     ┃\n");
+	printf("┃       12     ┃   N H     ┃\n");
+	printf("┃       13     ┃   신한    ┃\n");
+	printf("┃       14     ┃   우리    ┃\n");
+	printf("┃       15     ┃   하나    ┃\n");
+	printf("┃       16     ┃   외환    ┃\n");
+	printf("┃       17     ┃   씨티    ┃\n");
+	printf("┃       18     ┃   대구    ┃\n");
+	printf("┃       19     ┃   부산    ┃\n");
+	printf("┃       20     ┃   제일    ┃\n");
+	printf("┃       21     ┃   케이    ┃\n");
+	printf("┃       22     ┃  카카오   ┃\n");
+	printf("┃       30     ┃ 교통카드  ┃\n");
+	printf("┃       40     ┃  공과금   ┃\n");
+	printf("┃       41     ┃  범칙금   ┃\n");
+	printf("┗━━━━━━━━━━━━━━┻━━━━━━━━━━━┛\n");
+
+	return 0;
 }
